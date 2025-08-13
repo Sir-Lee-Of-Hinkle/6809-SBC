b34dca96d495f09fe668c7267ec4c722ced45826
Sir-Lee-Of-Hinkle <pannell212@protonmail.com>
2025-08-13 16:45:06 -0500

    Simplified for Troubleshooting

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -5,35 +5,63 @@
 ; PIA2 0xE004 - 0xE007
 ; ACIA 0xE008 - 0xE00F
 ; ROM 0xE010 - 0xFFFF
+PORTB = $E000
+PORTA = $E001
+DDRB = $E002
+DDRA = $E003
+
+PORTC = $E004
 PORTD = $E006
+PORTCCTL = $E005
 PORTDCTL = $E007
 E  = %10000000
+RW = %01000000
+RS = %00100000
 
 ; ########### Program Start/Reset ##############
 reset:
-  lds #$7FFF      ; declare the system stack. it grows downward
+  ;lds #$7FFF      ; declare the system stack. it grows downward
 
 ; ########## Test Code #######
   
-  lda #$55
-  sta [$1000]
+  ;lda #$55
+  ;sta [$1000]
   
-  lda #AA
-  sta > #$5000
+  ;lda #AA
+  ;sta > #$5000
   
+PIA1:
+  lda #%11111111 ; Set all pins on port B to output
+  sta DDRB
+
+  lda #%00000000 ; Set all pins on port A to input
+  sta DDRA
+  
+PIA2:
+  ; --- Configure Port C ---
+  ;LDA #$00
+  ;STA PORTCCTL           ; Control register to Direction
+
+  ;LDA #$00
+  ;STA PORTC              ; Set all to input
+
+  ;LDA #$04               ; #%00000111  CA1 low to high triggers IRQA
+  ;STA PORTCCTL           ; Control register to Data (bit 0-2 = 1)
+   
   ; --- Configure Port D ---
-  LDA #$00
-  STA PORTDCTL           ; Control register to Direction
+  ;LDA #$00
+  ;STA PORTDCTL           ; Control register to Direction
 
-  LDA #$FF
-  STA PORTD              ; Set all to output
+  ;LDA #$FF
+  ;STA PORTD              ; Set all to output
 
-  LDA #$04
-  STA PORTDCTL           ; Control register to Data (bit 2 = 1)
+  ;LDA #$04
+  ;STA PORTDCTL           ; Control register to Data (bit 2 = 1)
 
 
   lda #$55
-  sta PORTD
+  ;ta PORTB
+  ;sta PORTD
   nop
   nop
   nop
@@ -44,7 +72,8 @@ reset:
   nop
   
   lda #$AA
-  sta PORTD
+  sta PORTB
+  ;sta PORTD
   nop
   nop
   nop
@@ -57,7 +86,8 @@ reset:
   nop
   
   lda #$BB
-  sta PORTD
+  sta PORTB
+  ;sta PORTD
   nop
   nop
   nop
@@ -78,7 +108,7 @@ INIT:
   nop
   nop
   nop
-  bra INIT
+  jmp INIT
 
 
 ;###########################################################
